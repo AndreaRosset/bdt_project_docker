594c77327f746ab54453e0c5b7ffa8046ef454c2
Barbara <babinna96@gmail.com>
2020-07-01 21:46:16 +0200

    ho fatto un po' di commenti

--- dashboard/my_dash.py
+++ dashboard/my_dash.py
@@ -19,17 +19,18 @@ import plotly.express as px
 import plotly.offline as pyo
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
+import plotly.express as px
 from plotly import tools
-import psycopg2
+import psycopg2 # python package for Postgres
 
-conn = psycopg2.connect(host="postgresql", dbname="btc_sp500_stocks", user="postgres", password="postgres")
+conn = psycopg2.connect(host="postgresql", dbname="btc_sp500_stocks", user="postgres", password="postgres") #postgres connector
 sql = """SELECT * FROM btc_sp500_stocks"""
 cur = conn.cursor()
 cur.execute(sql)
 data = cur.fetchall()
 reduced_df = pd.DataFrame(data)
 #print(reduced_df)
-reduced_df.columns = ["t_time", "BTC", "SP500"]
+reduced_df.columns = ["t_time", "BTC", "SP500"] #Databse
 
 for index, value in enumerate(reduced_df["t_time"]):
     reduced_df["t_time"][index] = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(value))
@@ -37,20 +38,21 @@ for index, value in enumerate(reduced_df["t_time"]):
 reduced_df = reduced_df.iloc[1:]
 print(reduced_df)
 
+# Computing Moving Averages (the only one needed next is the BTC Moving Average)
 reduced_df["MA_100"] = reduced_df["SP500"].rolling(100,min_periods=10).mean().shift(1) #9 giorni
 reduced_df["MA_200"] = reduced_df["SP500"].rolling(200,min_periods=10).mean().shift(1) #21 giorni
 
 reduced_df["MAB_100"] = reduced_df["BTC"].rolling(100,min_periods=10).mean().shift()
 reduced_df["MAB_200"] = reduced_df["BTC"].rolling(200,min_periods=10).mean().shift()
 
-x = reduced_df.corr()
+x = reduced_df.corr() # Matrix Correlation
 print(x)
-correl = round(x["SP500"][0],4)
+correl = round(x["SP500"][0],4) # Correlation between BTC and S&P 500
 correl
 
-import plotly.express as px
 
-# Create traces moving averages
+
+# Create traces moving averages - Optional visualizations
 #masp500 = go.Figure()
 #masp500.add_trace(go.Scatter(x=reduced_df['t_time'], y=reduced_df['SP500'], mode='lines', name='S&P 500'))
 #masp500.add_trace(go.Scatter(x=reduced_df['t_time'], y=reduced_df['MA_100'], mode='lines', name='MA 100 records'))
@@ -63,45 +65,48 @@ import plotly.express as px
 #mabtc.add_trace(go.Scatter(x=reduced_df['t_time'], y=reduced_df['MAB_200'], mode='lines', name='MAB 200 records'))
 #mabtc.update_layout(title='BTC Moving Averages', xaxis_title='Time',)
 
-
-
-
 #masp500.show()
 #mabtc.show()
 
+
+# COMPUTATION: Imposing conditions on Correlation and Trend by comparing the BTC Moving Average and BTC Real Trend
 results = {}
 
-if correl == 1: #entrambi salgono/scendono : perfetta corr positiva
-    if reduced_df["MAB_100"].iloc[-1] < reduced_df["BTC"].iloc[-1]: #se btc sale
-        results=({"date":reduced_df["t_time"].iloc[-1],"correl":correl,"act":"strong long", "type":"aggressive"}) #prezzi a rialzo
-    else: # se btc scende mi devo difendere
-        results=({"date":reduced_df["t_time"].iloc[-1],"correl":correl,"act":"short", "type":"defensive"}) #prezzi a ribasso
+if correl == 1: # both increase/decrease in the same proportion  : perfect positive correlation
+    if reduced_df["MAB_100"].iloc[-1] < reduced_df["BTC"].iloc[-1]: # rising btc price
+        results=({"date":reduced_df["t_time"].iloc[-1],"correl":correl,"act":"Long"}) # Buy for sell later at a higher price
+    else: # decreasing btc price
+        results=({"date":reduced_df["t_time"].iloc[-1],"correl":correl,"act":"Short"}) # Sell for buy later at a lower price
 
-elif 0 < correl < 1: #entrambi salgono o scendono  ma la corr è più bassa
-    if reduced_df["MAB_100"].iloc[-1] < reduced_df["BTC"].iloc[-1]: # prezzi btc lenti a rialzo
-        results=({"date":reduced_df["t_time"].iloc[-1],"correl":correl,"act":"Long","type": "neutral"})
-    else: #prezzi btc lenti a ribasso
-        results=({"date":reduced_df["t_time"].iloc[-1],"correl":correl, "act":"short","type": "neutral"}) #prezzi lenti a ribasso
+elif 0 < correl < 1: # both increase/decrease but not in the same proportion : positive correlation
+    if reduced_df["MAB_100"].iloc[-1] < reduced_df["BTC"].iloc[-1]: # rising btc price
+        results=({"date":reduced_df["t_time"].iloc[-1],"correl":correl,"act":"Long"})
+    else: # decreasing btc price
+        results=({"date":reduced_df["t_time"].iloc[-1],"correl":correl, "act":"Short"}) # 
         
-elif correl -1 < correl < 0: #uno sale e uno scende con corr bassa 
-    if reduced_df["MAB_100"].iloc[-1] < reduced_df["BTC"].iloc[-1]: # BTC sale (sp scende)
-        results=({"date":reduced_df["t_time"].iloc[-1],"correl":correl, "act":"long","type": "neutral"})
-    else: # BTC scende (sp sale)
-        results=({"date":reduced_df["t_time"].iloc[-1],"correl":correl, "act":"short", "type":"neutral"})
+elif correl -1 < correl < 0: # Opposite trend between BTC and S&P 500 but not in the same proportion : negative correlation
+    if reduced_df["MAB_100"].iloc[-1] < reduced_df["BTC"].iloc[-1]: # BTC increases (sp500 decreases)
+        results=({"date":reduced_df["t_time"].iloc[-1],"correl":correl, "act":"Long"})
+    else: # BTC decreases (sp500 increases)
+        results=({"date":reduced_df["t_time"].iloc[-1],"correl":correl, "act":"Short"})
         
-elif correl == -1: # uno sale e uno scende : perfetta corr negativa
-    if reduced_df["MAB_100"].iloc[-1] < reduced_df["BTC"].iloc[-1]: #se btc sale (sp500 scende)
-        results=({"date":reduced_df["t_time"].iloc[-1],"correl":correl, "act":"strong long","type": "aggressive"})
-    else: # se btc scende (sp500 sale)
-        results=({"date":reduced_df["t_time"].iloc[-1],"correl":correl, "act":"long", "type":"defensive"})
+elif correl == -1: # Opposite trend between BTC and S&P 500 : perfect negative correlation
+    if reduced_df["MAB_100"].iloc[-1] < reduced_df["BTC"].iloc[-1]: # BTC increases (sp500 decreases)
+        results=({"date":reduced_df["t_time"].iloc[-1],"correl":correl, "act":"Long"})
+    else: # BTC decreases (sp500 increases)
+        results=({"date":reduced_df["t_time"].iloc[-1],"correl":correl, "act":"Short"})
 
-elif correl == 0: #x=0
-    results=({"date":reduced_df["t_time"].iloc[-1],"correl":"no correlation", "act":"non", "type":"non"})
+elif correl == 0: # No correlation
+    results=({"date":reduced_df["t_time"].iloc[-1],"correl":"no correlation", "act":"None"})
 else:
-    results=({"date":"error","correl":"error", "act":"error", "type":"error"})
+    results=({"date":"Error","correl":"Error", "act":"Error"})
 
 import plotly.graph_objects as go
 
+
+# Creating figures for the Dash
+
+# fig: S&P 500 and BTC are displayed together in a unique graph, so it is needed a second y-axis 
 fig = go.Figure()
 
 fig.add_trace(go.Scatter(
@@ -132,7 +137,7 @@ fig.update_layout(
     yaxis=dict(
         title="S&P 500 Price",
         titlefont=dict(
-            color="orange" ##1f77b4
+            color="orange" 
         ),showgrid = False,showspikes= True,spikedash= 'dot',spikemode= 'across',spikesnap= 'cursor',
         tickfont=dict(
             color="orange"
@@ -141,7 +146,7 @@ fig.update_layout(
     yaxis2=dict(
         title="BTC Price",
         titlefont=dict(
-            color="#00FE35" ##ff7f0e
+            color="#00FE35" 
         ),
         tickfont=dict(
             color="#00FE35"
@@ -149,26 +154,26 @@ fig.update_layout(
         anchor="x",
         overlaying="y",
         side="right",
-        #position=0.15
     ),
 
 )
 
-# Update layout properties
+# Update layout properties of fig
 fig.update_layout(title_text="S&P 500 and BTC Prices", plot_bgcolor='rgb(10,10,10)',paper_bgcolor='black',
                  font=dict(
                     color = 'yellow',
                      size=18
                  )
-                 ),
+                 )
 
 
+# fig1: displaying BTC
 fig1 = go.Figure()
 fig1.add_trace(go.Scatter(x=reduced_df['t_time'], y=reduced_df['BTC'],mode='lines',name='BTC',
                           line=dict(color='#00FE35'),))
                           
               
-
+# Updating fig1 (btc) layout
 fig1.update_layout(plot_bgcolor='rgb(10,10,10)',paper_bgcolor='black', clickmode= 'event+select',
                    xaxis=dict(title='Time', titlefont=dict(color='yellow',size=12),
                               showspikes= True, color='yellow',showgrid = False,
@@ -181,12 +186,13 @@ fig1.update_layout(plot_bgcolor='rgb(10,10,10)',paper_bgcolor='black', clickmode
 
 
 
+# fig2: displaying S&P 500
 fig2 = go.Figure()
 fig2.add_trace(go.Scatter(x=reduced_df['t_time'], y=reduced_df['SP500'],mode='lines',name='BTC',
                           line=dict(color='orange'),))
                           
               
-
+# Updating fig2 (sp500) layout
 fig2.update_layout(plot_bgcolor='rgb(10,10,10)',paper_bgcolor='black', clickmode= 'event+select',
                    xaxis=dict(title='Time', titlefont=dict(color='yellow',size=12),
                               showspikes= True,color='yellow',showgrid = False,
@@ -198,25 +204,19 @@ fig2.update_layout(plot_bgcolor='rgb(10,10,10)',paper_bgcolor='black', clickmode
                   title='S&P 500 Price', titlefont=dict(color='orange',size=18))
 
 
+# RESULTS
+# Creating the Plotly Dash for visualize results of conditions imposed on correlation and graphs
+
 import flask
 
-#from jupyterlab_dash import AppViewer
-#viewer = AppViewer()
-
-#server = flask.Flask(__name__) # define flask app.server
-#app = dash.Dash(__name__, server=server) # call flask server
-#gunicorn my_dash:app.server -b :8000
 app = dash.Dash()
 app.layout = html.Div()
-
-
 
 
 all_options = {
     'DateTime': [results["date"]],
     'Correlation': [results['correl']],
-    'Act': [results['act']],
-    'Type': [results['type']]
+    'Act': [results['act']]
 }
 
 
@@ -224,7 +224,7 @@ app.layout = html.Div(children=[
     html.Div(className='row',  # Define the row element
                             style={
                                       'textAlign': 'center',
-                                      "background": "DimGray", #DimGray
+                                      "background": "DimGray", 
                                       'fontWeight': 'bold',
                                        'color': 'black'},
                                
@@ -238,7 +238,7 @@ app.layout = html.Div(children=[
                                    ]),
                         html.Hr(),
                         
-                        html.Div([
+                        html.Div([ # Define RadioItems 
                                 dcc.RadioItems(
                                 id='results-dropdown',
                                     style={'marginTop': 0, 'marginBottom': 0},
@@ -251,7 +251,7 @@ app.layout = html.Div(children=[
                             html.Div(id='display-selected-values')
                         ]),
                                    
-
+    # Define Dropdown
     html.Div(className='drop-dwn',children=[
         dcc.Dropdown(id='drop-indicator-px',
                 multi = False,
@@ -263,19 +263,20 @@ app.layout = html.Div(children=[
                 placeholder='Select graph'),
     ]),
     
+    # Define (space for) graph
     html.Div(className='graph',children=[
                 dcc.Graph(id='timeseries')
     ])
 ])
 ])      
 
-                
+# Making Dashboard Interactive using callbacks
+
+# Callback for visualize graphs one at time       
 @app.callback(
     dash.dependencies.Output('timeseries', 'figure'),
     [dash.dependencies.Input('drop-indicator-px', 'value')])   
-def up_image(selector):
-    #data=[]
-    #figure=go.Figure()
+def up_image(selector): 
    
     try:
 
@@ -297,7 +298,7 @@ def up_image(selector):
     return figure
     
     
-    
+# Two callback for RadioItems results and options
 @app.callback(
     dash.dependencies.Output('opt-dropdown', 'options'),
     [dash.dependencies.Input('results-dropdown', 'value')])
@@ -317,4 +318,4 @@ def set_results_value(available_options):
 if __name__ == "__main__" :
     app.run_server(debug=True, use_reloader=False, host="0.0.0.0")         
     
-#viewer.show(app)
+# use_reloader = False since we used Jupyter
